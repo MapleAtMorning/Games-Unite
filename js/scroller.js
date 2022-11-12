5508994e38473bcc5623713406c0ad793b8e7795
MapleAtMorning <flame.band.skeleton@gmail.com>
2022-11-12 14:53:37 -0600

    More sizing & mobile support
    Maple makes worst JavaScript ever, asked to stop development.

--- js/scroller.js
+++ js/scroller.js
@@ -1,21 +1,29 @@
 function scrollFunc(id) {
+    let sidebar = document.getElementById('sidebarContent');
+    let openedCanvas = bootstrap.Offcanvas.getInstance(sidebar);
     let element = document.getElementById(id);
+
+    openedCanvas.toggle(); 
+
+    setTimeout(() => {
+        element.scrollIntoView({behavior:"smooth", block:"center"});
+        element.classList.add('playOut');
+        element.addEventListener("animationend", () => {
     
-    element.classList.add('playOut')
-    element.scrollIntoView({behavior:"smooth", block:"center"});
-    element.addEventListener("animationend", () => {
-
-        element.style.letterSpacing = "0.3em";
-        element.classList.remove('playOut');
-
-        setTimeout(() => {
-            element.classList.add('playIn');
+            element.style.letterSpacing = "0.3em";
+            element.classList.remove('playOut');
+    
             setTimeout(() => {
-                element.classList.remove('playIn');
-                element.style.letterSpacing = "0em";
-            }, 1001); 
-            
-        }, 1000);         
-    });
+                element.classList.add('playIn');
+                setTimeout(() => {
+                    element.classList.remove('playIn');
+                    element.style.letterSpacing = "0em";
+                }, 1001); 
+                
+            }, 1000);       
+        });
+    }, 350);
+
+    //! this is hell and unreadable I do not know javascript well, this was self taught and very very rough
 
 }
