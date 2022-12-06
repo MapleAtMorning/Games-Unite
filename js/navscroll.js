dabeaeda8d294e4a1fe33a807e546e370290dc9c
MapleAtMorning <79129529+MapleAtMorning@users.noreply.github.com>
2022-12-06 09:52:14 -0600

    Nav Fading Refinement
    Made the fading on index for the navbar better. Loving how it looks.

--- js/navscroll.js
+++ js/navscroll.js
@@ -1,11 +1,15 @@
 const nav = document.getElementById('navTop');
+const logo = document.getElementById('headerLogo')
 window.onscroll = function(){scrollFade()};
-
 
 function scrollFade(){
     if (document.documentElement.scrollTop < 100) {
         nav.style.backgroundColor = '#1d252900'; // When at top of page
+        nav.style.textShadow = '0.2rem 0.2rem 0.2rem #000'
+        logo.style.filter = 'drop-shadow(0.2rem 0.2rem 0.2rem #000)'
     } else {
-        nav.style.backgroundColor = '#1d2529ff'; // When at top of page
+        nav.style.backgroundColor = '#1d2529ff'; // When NOT at top of page
+        nav.style.textShadow = '0 0 0 #000'
+        logo.style.filter = 'drop-shadow(0 0 0 #000)'
     }
 };
