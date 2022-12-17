7476389209e842557638f705ee811e09cb0df1f8
MapleAtMorning <79129529+MapleAtMorning@users.noreply.github.com>
2022-12-16 20:42:09 -0600

    Finished themes

--- js/cookiehandler.js
+++ js/cookiehandler.js
@@ -1,4 +1,10 @@
-function setCookie(name, value) {
+const itemHolder = document.getElementById('nav-item-holder');
+const header = document.getElementById('navTop');
+let theme = getCookie("theme");
+const root = document.querySelector(':root');
+const rootstyle = getComputedStyle(root);
+
+export function setCookie(name, value) {
     const d = new Date();
     d.setTime(d.getTime() + (365 * 24 * 60 * 60 * 1000));
     let expires = "expires=" + d.toUTCString();
@@ -7,7 +13,7 @@ function setCookie(name, value) {
     document.cookie = name + "=" + value + ";" + expires + "; SameSite=Strict";
 }
 
-function getCookie(cname) {
+export function getCookie(cname) {
     let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
     let cookieList = decodedCookie.split(';');
@@ -25,12 +31,12 @@ function getCookie(cname) {
 // HEAVILY HEAVILY LEARNT AND COPIED FROM https://www.w3schools.com/js/js_cookies.asp
 
 function cookieChecker(){
+    const modal = new bootstrap.Modal(document.getElementById('cookiesModal'));
     if(getCookie("cookiesApproved")){
         return;
     }
     // Check if cookies have already been approved, if so return so code below DOESN'T RUN.
 
-    const modal = new bootstrap.Modal(document.getElementById('cookiesModal'));
     modal.show();
     // Simply bring up the cookies modal popup.
 
@@ -43,7 +49,6 @@ function cookieChecker(){
 }
 
 function themeColorUpdater(primary, primarylight, background, secondary, secondarydark, text, mute){
-    const root = document.querySelector(':root');
     root.style.setProperty("--primaryColor", primary)
     root.style.setProperty("--primaryLight", primarylight)
     root.style.setProperty("--primaryBackground", background)
@@ -53,33 +58,109 @@ function themeColorUpdater(primary, primarylight, background, secondary, seconda
     root.style.setProperty("--muteText", mute)
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    if(getCookie("theme")){
-        let theme = getCookie("theme");
-
+function themeChanger(){
+    theme = getCookie("theme");
+    if(getCookie("theme")){   
+        
+        if(theme != "light"){header.classList.remove("light-theming");}
+        
         if(theme == "contrast"){
             themeColorUpdater(
                 "#000000", //primary
-                "#ffffff", //primaryLight
+                "#585858", //primaryLight
                 "#000000", //primaryBackground
                 "#4bf5a3", //secondary
                 "#46aa7a", //secondaryDark
                 "#ffffff", //text
                 "#ffffff" //mute
             )
+            header.classList.remove("navbar-light");
+            header.classList.add("navbar-dark");
+            header.classList.remove('light-theming');
+            header.classList.add('default-theming');
+            itemHolder.classList.remove('light-theming');
+            itemHolder.classList.remove('default-theming');
+        }
+        else if(theme == "default"){
+            themeColorUpdater(
+                "#1d2529", //primary
+                "#283035", //primaryLight
+                "#181d1f", //primaryBackground
+                "#4bf5a3", //secondary
+                "#46aa7a", //secondaryDark
+                "#ffffff", //text
+                "#6c757d" //mute
+            )
+            header.classList.remove("navbar-light");
+            header.classList.add("navbar-dark");
+            header.classList.remove('light-theming');
+            header.classList.add('default-theming');
+            itemHolder.classList.remove('light-theming');
+            itemHolder.classList.remove('default-theming');
         }
         else if(theme == "light"){
             themeColorUpdater(
                 "#ECEEF4", //primary
-                "#000000", //primaryLight
+                "#ccd0db", //primaryLight
                 "#ffffff", //primaryBackground
                 "#4bf5a3", //secondary
                 "#46aa7a", //secondaryDark
                 "#000000", //text
                 "#6c757d" //mute
             )
+            header.classList.remove("navbar-dark");
+            header.classList.add("navbar-light");
+            header.classList.add('light-theming');
+            header.classList.remove('default-theming');
         }
+        header.style.backgroundColor = rootstyle.getPropertyValue('--primaryColor');
     }
-    
-    cookieChecker();
+}
+
+// Wizardry with the light theme to make it always have proper light navigation on mobile
+export function mobileLight(){
+    let theme = getCookie("theme");
+    if(theme == "light"){
+        header.classList.remove('default-theming');
+        header.classList.add('light-theming');
+        if(window.innerWidth < 992){
+            
+            itemHolder.classList.remove('default-theming');
+            itemHolder.classList.add('light-theming');
+            return;
+        }
+        itemHolder.classList.remove('light-theming');
+        itemHolder.classList.add('default-theming');
+    }
+}
+
+// Check when radio buttons are clicked to change theme
+const colorThemes = document.querySelectorAll('[name="theme"]');
+colorThemes.forEach((themeOption) => {
+    themeOption.addEventListener("click", () => {
+        setCookie("theme", themeOption.id);
+        themeChanger();
+    });
 });
+
+const setTheme = function () {
+    const activeTheme = getCookie("theme");
+    colorThemes.forEach((themeOption) => {
+        if (themeOption.id === activeTheme) {
+            themeOption.checked = true;
+        }
+    });
+    // fallback for no :has() support
+    document.documentElement.className = activeTheme;
+};
+
+// This code has been taken from Kevin Powell's great theme selector with :has() tutorial
+
+window.onresize = mobileLight;
+themeChanger();
+mobileLight();
+if(window.location.pathname == "/"){
+    cookieChecker();
+}
+console.log(window.location.pathname)
+document.onload = setTheme();
--- js/navfade.js
+++ js/navfade.js
@@ -1,26 +1,48 @@
+import {getCookie, mobileLight} from "./cookiehandler.js";
+
 const nav = document.getElementById('navTop');
 const logo = document.getElementById('headerLogo');
-
+const header = document.getElementById('navTop');
+const itemHolder = document.getElementById('nav-item-holder');
 const root = document.querySelector(':root');
 const rootstyle = getComputedStyle(root);
 
-const navLinks = document.getElementsByClassName('nav-link');
-
-let primary = rootstyle.getPropertyValue('--primaryColor');
 window.onscroll = function(){scrollFade()};
-
+window.onresize = scrollFade;
+scrollFade();
 
 function scrollFade(){
     let primary = rootstyle.getPropertyValue('--primaryColor');
-    if (document.documentElement.scrollTop < 100) { // When at top of page
-        nav.style.backgroundColor = primary+'00'; 
-        nav.style.textShadow = '0.2rem 0.2rem 0.2rem #000';
-        logo.style.filter = 'drop-shadow(0.2rem 0.2rem 0.2rem #000)';
+    let theme = getCookie("theme");
 
+    if (document.documentElement.scrollTop < 100)  { // When at top of page
+        nav.style.backgroundColor = primary+'00'; 
+
+        if(theme == "light"){
+            mobileLight()
+            header.classList.remove('light-theming');
+            header.classList.add('default-theming');
+            if(window.innerWidth < 992){
+                shadow("0 ");
+                return;
+            }
+        }
+
+        shadow("0.2rem ");
     } else { // When NOT at top of page
         nav.style.backgroundColor = primary+'ff'; 
-        nav.style.textShadow = '0 0 0 #000';
-        logo.style.filter = 'drop-shadow(0 0 0 #000)';
-        
+        if(theme == "light"){
+            mobileLight()
+            if(window.innerWidth < 992){
+                shadow("0 ");
+                return;
+            }
+        }
+        shadow("0 ");
     }
 };
+
+function shadow(distance){
+    nav.style.textShadow = distance + distance + distance + '#000';
+    logo.style.filter = 'drop-shadow(' + distance + distance + distance + '#000)';
+}
