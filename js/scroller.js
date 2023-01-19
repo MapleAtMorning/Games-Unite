a1b7aa6b3c6f81e4cc4ea83fde46798a5c7c2bd1
MapleAtMorning <79129529+MapleAtMorning@users.noreply.github.com>
2023-01-19 16:12:24 -0600

    Finished up copy popup
    Should be ready for merge with main.

--- js/scroller.js
+++ js/scroller.js
@@ -1,9 +1,11 @@
+const sidebar = document.getElementById('sidebarContent');
+const openedCanvas = bootstrap.Offcanvas.getInstance(sidebar);
+let timeDelay
+
 function scrollFunc(id) {
-    const sidebar = document.getElementById('sidebarContent');
-    const openedCanvas = bootstrap.Offcanvas.getInstance(sidebar);
-    const vw = window.innerWidth;
+    let vw = window.innerWidth;
     let element = document.getElementById(id);
-    let timeDelay
+
 
     if(vw < 992) {	
         openedCanvas.hide(); 	
@@ -16,18 +18,15 @@ function scrollFunc(id) {
         element.scrollIntoView({behavior:"smooth", block:"center"});
         element.classList.add('play-out');
         element.addEventListener("animationend", () => {
-    
+
             element.style.letterSpacing = "0.3em";
             element.classList.remove('play-out');
-    
             setTimeout(() => {
                 element.classList.add('play-in');
-                
                 setTimeout(() => {
                     element.classList.remove('play-in');
                     element.style.letterSpacing = "0em";
-                }, 1001); 
-                
+                }, 1001);  
             }, 1000);       
         });
     }, timeDelay);
