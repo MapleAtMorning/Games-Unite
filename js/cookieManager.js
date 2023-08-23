892de06f2622ffab1c686b523275f3244222a937
Maple <79129529+MapleAtMorning@users.noreply.github.com>
2023-08-23 10:40:44 -0500

    Start of cookies JS

---
+++ js/cookieManager.js
@@ -0,0 +1,18 @@
+const acceptButton = document.getElementById("cookies-button")
+const cookiesAlert = document.getElementById("cookies-modal")
+const ls = window.localStorage
+
+acceptButton.addEventListener("click", () => {
+    acceptButton.disabled = true
+    cookiesAlert.classList.add("cookies-anim-out")
+
+    ls.setItem("GUCookiesAccepted", "true")
+
+    cookiesAlert.addEventListener("animationend", () => {
+        cookiesAlert.remove()
+    })
+})
+
+if (ls.getItem("GUCookiesAccepted" !== null)){
+    console.log("Cookies Accepted")
+}
